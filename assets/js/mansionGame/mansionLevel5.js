bbc0854e622959248cfc6a5ed8623afd29e10bf5
Grot710 <gr00tisbest@gmail.com>
2025-11-03 09:13:55 -0800

    20 zombies again :(

--- assets/js/mansionGame/mansionLevel5.js
+++ assets/js/mansionGame/mansionLevel5.js
@@ -535,7 +535,7 @@ class MansionLevel5 {
             <div>
             'W','A','S','D' to move around <br>
             'C' to shoot <br>
-            Kill 15 zombies to progress <br>
+            Kill 20 zombies to progress <br>
             Press 'E' to enter the door at the end
             </div>
         `
@@ -550,7 +550,7 @@ class MansionLevel5 {
                 <div style="font-size: 14px;">${this.zombiesKilled} zombie kills</div>
             `;
 
-            if (this.zombiesKilled >= 15 && this.spawnZombies) 
+            if (this.zombiesKilled >= 20 && this.spawnZombies) 
             {
                 console.log("Level completed")
                 this.spawnZombies = false;
