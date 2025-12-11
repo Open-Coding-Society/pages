a4461d061658bd885d8838bdea2bedee3f835de6
jm1021 <jmort1021@gmail.com>
2025-12-11 11:10:54 -0800

    fixed game level

--- assets/js/adventureGame/GameEngine/Coin.js
+++ assets/js/adventureGame/GameEngine/Coin.js
@@ -1,5 +1,7 @@
-import { Transform } from "./Transform";
+import { Transform } from "./Transform.js";
+import GameObject from './GameObject.js';
 
+// Low-level utility Coin class (kept for compatibility)
 export class Coin {
     constructor(x, y, points, size) {
         this.transform = new Transform(x, y);
@@ -20,3 +22,41 @@ export class Coin {
         ctx.fillRect(this.transform.x + canvas.width/2, this.transform.y + canvas.height/2, this.size, this.size);
     }
 };
+
+// GameObject-compatible wrapper so coins can be used in GameLevel classes
+export default class CoinObject extends GameObject {
+    constructor(data = {}, gameEnv = null) {
+        super(gameEnv);
+        this.spriteData = data;
+        // Position on canvas (defaults near top-left)
+        this.x = (data.INIT_POSITION && data.INIT_POSITION.x) || (data.x || 50);
+        this.y = (data.INIT_POSITION && data.INIT_POSITION.y) || (data.y || 50);
+        this.points = data.points || 1;
+        this.size = data.size || 16;
+        this.hitbox = data.hitbox || { widthPercentage: 0.0, heightPercentage: 0.0 };
+    }
+
+    update() {
+        this.draw();
+    }
+
+    draw() {
+        const ctx = this.gameEnv && this.gameEnv.ctx;
+        if (!ctx) return;
+        ctx.fillStyle = this.spriteData.color || 'yellow';
+        // draw a simple square coin; position is absolute canvas coords
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+
+    resize() {
+        // Nothing special to do on resize for the coin
+        this.draw();
+    }
+
+    destroy() {
+        const index = this.gameEnv.gameObjects.indexOf(this);
+        if (index !== -1) {
+            this.gameEnv.gameObjects.splice(index, 1);
+        }
+    }
+}
