d00c675c1d00b7b7a3220b0e4106f0b19631381e
Ahmad-Se-tech <ahmad_sediqi3@hotmail.com>
2026-02-12 09:23:09 -0800

    coded and fixed issues

--- assets/js/murderMystery/MurderMysteryL6.js
+++ assets/js/murderMystery/MurderMysteryL6.js
@@ -4,71 +4,65 @@ import Boss from '/assets/js/GameEnginev1/essentials/Boss.js';
 
 class MurderMysteryBossFight {
   constructor(gameEnv) {
-    let width = gameEnv.innerWidth;
-    let height = gameEnv.innerHeight;
-    let path = gameEnv.path;
+    this.gameEnv = gameEnv;
+    const path = gameEnv.path;
 
-    const image_background = path + "/images/murderMystery/boss_map.png"; 
     const image_data_background = {
-        name: 'background',
-        greeting: "The story begins here, you will search for clues and solve the mystery of this murder.",
-        src: image_background,
-        pixels: {height: 580, width: 1038},
-        mode: 'contain',
+      name: 'background',
+      greeting: "The story begins here, you will search for clues and solve the mystery of this murder.",
+      src: path + "/images/murderMystery/boss_map.png",
+      pixels: { height: 580, width: 1038 },
+      mode: 'contain',
     };
-
 
     const sprite_data_archie = {
-        id: 'Archie',
-        greeting: "Hi, I am Archie.",
-        src: path + "/images/murderMystery/archie_left.png",
-        SCALE_FACTOR: 4,
-        STEP_FACTOR: 1000,
-        ANIMATION_RATE: 0,
-        INIT_POSITION: { x: 250, y: 350 },
-        pixels: {height: 150, width: 100},
-        orientation: {rows: 1, columns: 1},
-        down: {row: 0, start: 0, columns: 1},
-        downRight: {row: 0, start: 0, columns: 1},
-        downLeft: {row: 0, start: 0, columns: 1},
-        left: {row: 0, start: 0, columns: 1},
-        right: {row: 0, start: 0, columns: 1},
-        up: {row: 0, start: 0, columns: 1},
-        upLeft: {row: 0, start: 0, columns: 1},
-        upRight: {row: 0, start: 0, columns: 1},
-        hitbox: {widthPercentage: 0.5, heightPercentage: 0.5},
-        keypress: {left: 65, right:68, up: 87, down: 83} // A, D, W, S
+      id: 'Archie',
+      greeting: "Hi, I am Archie.",
+      src: path + "/images/murderMystery/archie_left.png",
+      SCALE_FACTOR: 4,
+      STEP_FACTOR: 1000,
+      ANIMATION_RATE: 0,
+      INIT_POSITION: { x: 250, y: 350 },
+      pixels: { height: 150, width: 100 },
+      orientation: { rows: 1, columns: 1 },
+      down: { row: 0, start: 0, columns: 1 },
+      downRight: { row: 0, start: 0, columns: 1 },
+      downLeft: { row: 0, start: 0, columns: 1 },
+      left: { row: 0, start: 0, columns: 1 },
+      right: { row: 0, start: 0, columns: 1 },
+      up: { row: 0, start: 0, columns: 1 },
+      upLeft: { row: 0, start: 0, columns: 1 },
+      upRight: { row: 0, start: 0, columns: 1 },
+      hitbox: { widthPercentage: 0.5, heightPercentage: 0.5 },
+      keypress: { left: 65, right: 68, up: 87, down: 83 } // A, D, W, S
     };
 
-
     const sprite_data_boss = {
-        id: 'Boss',
-        src: path + "/images/murderMystery/bossleft.png",
-        SCALE_FACTOR: 2,
-        STEP_FACTOR: 1000, // Same speed as Archie
-        ANIMATION_RATE: 0,
-        INIT_POSITION: { x: 850, y: 400 }, 
-        pixels: { height: 200, width: 400 },
-        orientation: { rows: 1, columns: 1 },
-        down: { row: 0, start: 0, columns: 1 },
-        downRight: {row: 0, start: 0, columns: 1},
-        downLeft: {row: 0, start: 0, columns: 1},
-        left: { row: 0, start: 0, columns: 1 },
-        right: { row: 0, start: 0, columns: 1 },
-        up: { row: 0, start: 0, columns: 1 },
-        upLeft: {row: 0, start: 0, columns: 1},
-        upRight: {row: 0, start: 0, columns: 1},
-        hitbox: { widthPercentage: 0.8, heightPercentage: 0.8 },
-   };
-
+      id: 'Boss',
+      src: path + "/images/murderMystery/bossleft.png",
+      SCALE_FACTOR: 2,
+      STEP_FACTOR: 1000,
+      ANIMATION_RATE: 0,
+      INIT_POSITION: { x: 850, y: 400 },
+      pixels: { height: 200, width: 400 },
+      orientation: { rows: 1, columns: 1 },
+      down: { row: 0, start: 0, columns: 1 },
+      downRight: { row: 0, start: 0, columns: 1 },
+      downLeft: { row: 0, start: 0, columns: 1 },
+      left: { row: 0, start: 0, columns: 1 },
+      right: { row: 0, start: 0, columns: 1 },
+      up: { row: 0, start: 0, columns: 1 },
+      upLeft: { row: 0, start: 0, columns: 1 },
+      upRight: { row: 0, start: 0, columns: 1 },
+      hitbox: { widthPercentage: 0.8, heightPercentage: 0.8 },
+    };
 
     this.classes = [
-            { class: GameEnvBackground, data: image_data_background },
-            { class: Boss, data: sprite_data_boss },  
-            { class: Player, data: sprite_data_archie }, 
-        ];
-   
-}
+      { class: GameEnvBackground, data: image_data_background },
+      { class: Boss, data: sprite_data_boss },
+      { class: Player, data: sprite_data_archie },
+    ];
+  }
 }
 
 export default MurderMysteryBossFight;
