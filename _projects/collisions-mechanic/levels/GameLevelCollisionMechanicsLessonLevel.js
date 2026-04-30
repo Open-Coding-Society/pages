751bc18c56f919ae7d835ff6b0144a071bea6890
dragon-rg <rigved.me@gmail.com>
2026-04-29 23:30:14 -0700

    progress

--- _projects/collisions-mechanic/levels/GameLevelCollisionMechanicsLessonLevel.js
+++ _projects/collisions-mechanic/levels/GameLevelCollisionMechanicsLessonLevel.js
@@ -14,7 +14,8 @@ import GameEnvBackground from '@assets/js/GameEnginev1/essentials/GameEnvBackgro
 import Player from '@assets/js/GameEnginev1/essentials/Player.js';
 import Npc from '@assets/js/GameEnginev1/essentials/Npc.js';
 import Barrier from '@assets/js/GameEnginev1/essentials/Barrier.js';
-import Enemy from '@assets/js/GameEnginev1/essentials/Enemy.js';
+import Shark from '@assets/js/GameEnginev1/Shark.js';
+import ExampleEnemy from '@assets/js/projects/collisions-mechanic/levels/ExampleEnemy.js';
 
 class GameLevelCollisionMechanicsLessonLevel {
     constructor(gameEnv) {
@@ -49,31 +50,8 @@ class GameLevelCollisionMechanicsLessonLevel {
             keypress: { up: 87, left: 65, down: 83, right: 68 }
             };
 
-        const npcData1 = {
-            id: 'ufo_1',
-            greeting: 'Hello!',
-            src: path + "/images/gamebuilder/sprites/ufos.png",
-            SCALE_FACTOR: 8,
-            ANIMATION_RATE: 50,
-            INIT_POSITION: { x: 500, y: 300 },
-            pixels: { height: 500, width: 500 },
-            orientation: { rows: 4, columns: 3 },
-            down: { row: 0, start: 0, columns: 3 },
-            right: { row: Math.min(1, 4 - 1), start: 0, columns: 3 },
-            left: { row: Math.min(2, 4 - 1), start: 0, columns: 3 },
-            up: { row: Math.min(3, 4 - 1), start: 0, columns: 3 },
-            upRight: { row: Math.min(3, 4 - 1), start: 0, columns: 3 },
-            downRight: { row: Math.min(1, 4 - 1), start: 0, columns: 3 },
-            upLeft: { row: Math.min(2, 4 - 1), start: 0, columns: 3 },
-            downLeft: { row: 0, start: 0, columns: 3 },
-            hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
-            dialogues: ['Hello!'],
-            reaction: function() { if (this.dialogueSystem) { this.showReactionDialogue(); } else { console.log(this.greeting); } },
-            interact: function() { if (this.dialogueSystem) { this.showRandomDialogue(); } }
-        };
-
-        const npcData2 = {
-            id: 'ufo_2',
+        const npcData = {
+            id: 'ufo',
             greeting: 'Hello!',
             src: path + "/images/gamebuilder/sprites/ufos.png",
             SCALE_FACTOR: 8,
@@ -90,15 +68,13 @@ class GameLevelCollisionMechanicsLessonLevel {
             upLeft: { row: Math.min(2, 4 - 1), start: 0, columns: 3 },
             downLeft: { row: 0, start: 0, columns: 3 },
             hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
-            dialogues: ['Hello!'],
-            reaction: function() { if (this.dialogueSystem) { this.showReactionDialogue(); } else { console.log(this.greeting); } },
-            interact: function() { if (this.dialogueSystem) { this.showRandomDialogue(); } }
         };
+
+
         this.classes = [      
             { class: GameEnvBackground, data: bgData },
             { class: Player, data: playerData },
-            { class: Enemy, data: npcData1 },
-            { class: Enemy, data: npcData2 }
+            { class: ExampleEnemy, data: npcData }
         ];
     }
 }
