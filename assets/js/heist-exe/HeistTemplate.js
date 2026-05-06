50a10a40847823d1b808b3d0f23198d97b3e9963
MOOPA_01 <seonyoo.pak@gmail.com>
2026-05-06 15:06:20 -0700

    spline lvl ig

--- assets/js/heist-exe/HeistTemplate.js
+++ assets/js/heist-exe/HeistTemplate.js
@@ -24,7 +24,7 @@ class HeistTemplate {
         id: 'MC',
         name: 'mainplayer',
         src: path + "/assets/js/heist-exe/images/heist-mc.png",
-        //SCALE_FACTOR: MC_SCALE_FACTOR,
+        SCALE_FACTOR: 1000,
         STEP_FACTOR: 1000,
         ANIMATION_RATE: 50,
         //GRAVITY: true,
@@ -43,16 +43,86 @@ class HeistTemplate {
         keypress: {up: 87, left: 65, down: 83, right: 68}
     };
 
-     const barrier_1 = {
-            id: 'barrier_1',
-            x: width * 0.1,
-            y: width * 0.1,
-            width: width * 0.2,
-            height: width * 0.2, 
-            visible: true /* BUILDER_DEFAULT */,
-            hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 },
-            fromOverlay: true
-        };
+    const border_top = {
+        id: 'border_top',
+        x: 0.0, y: 0.0, width: 1.0, height: 0.09,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    } 
+
+    const border_bottom = {
+        id: 'border_bottom',
+        x: 0.0, y: 0.91, width: 1.0, height: 0.09,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+    const border_left = {
+        id: 'border_left',
+        x: 0.0, y: 0.0, width: 0.04, height: 1.0,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+    const border_right = {
+        id: 'border_right',
+        x: 0.96, y: 0.0, width: 0.04, height: 1.0,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+    const green_zone = {
+        id: 'green_zone',
+        x: 0.82, y: 0.09, width: 0.14, height: 0.82,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+    const wall_top_left = {
+        id: 'wall_top_left',
+        x: 0.04, y: 0.10, width: 0.27, height: 0.20,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+    const wall_top_right = {
+        id: 'wall_top_right',
+        x: 0.55, y: 0.10, width: 0.22, height: 0.16,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+    const wall_mid_left = {
+        id: 'wall_mid_left',
+        x: 0.04, y: 0.43, width: 0.13, height: 0.20,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+    const wall_mid_center = {
+        id: 'wall_mid_center',
+        x: 0.28, y: 0.43, width: 0.22, height: 0.16,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+    const wall_mid_right = {
+        id: 'wall_mid_right',
+        x: 0.55, y: 0.43, width: 0.22, height: 0.16,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
 
     const gem_data_1 = {
         id: 'gem1',
@@ -70,12 +140,39 @@ class HeistTemplate {
         INIT_POSITION: { x: width * 0.5, y: height * 0.5 }
     };
 
-    this.classes = [      
+    const wall_bot_left = {
+        id: 'wall_bot_left',
+        x: 0.04, y: 0.67, width: 0.27, height: 0.20,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+    const wall_bot_right = {
+        id: 'wall_bot_right',
+        x: 0.37, y: 0.67, width: 0.22, height: 0.16,
+        color: 'rgba(0, 255, 136, 0.5)',
+        visible: true,
+        hitbox: { widthPercentage: 0.0, heightPercentage: 0.0 }
+    }
+
+this.classes = [      
       { class: GameEnvBackground, data: image_data_bg },
       { class: Player, data: sprite_data_mc },
       { class: Gem, data: gem_data_1 },
-      { class: Gem, data: gem_data_2},
-      { class: Barrier, data: barrier_1}
+      { class: Gem, data: gem_data_2 },
+      { class: Barrier, data: border_top },
+      { class: Barrier, data: border_bottom },
+      { class: Barrier, data: border_left },
+      { class: Barrier, data: border_right },
+      { class: Barrier, data: green_zone },
+      { class: Barrier, data: wall_top_left },
+      { class: Barrier, data: wall_top_right },
+      { class: Barrier, data: wall_mid_left },
+      { class: Barrier, data: wall_mid_center },
+      { class: Barrier, data: wall_mid_right },
+      { class: Barrier, data: wall_bot_left },
+      { class: Barrier, data: wall_bot_right },
     ];
   }
 }
